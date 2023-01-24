ae1253762e1a4aee0f0a861064972811a538abe0
Ariestellar <ahdrei47@gmail.com>
2023-01-24 23:13:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from flask_ngrok import run_with_ngrok
 import git
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Запустить ngrok при запуске приложения, нужен для проброски тоннеля сервера
 logging.basicConfig(level=logging.DEBUG)
+
+
+# run_with_ngrok(app)  # Запустить ngrok при запуске приложения, нужен для проброски тоннеля сервера
+# if __name__ == '__main__':
+#     app.run()
 
 
 def make_response(text):
@@ -47,56 +51,11 @@ def main():
     else:
         return fallback()
 
-#git remote add origin https://github.com/Ariestellar/PYAlice.git
+
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        git.Repo()
+        git.cmd.Git().pull('https://github.com/Ariestellar/PYAlice', 'main')
         return 'Updated PythonAnywhere successfully', 200
     else:
         return 'Wrong event type', 400
-
-
-# request = {
-#     "meta": {
-#         "locale": "ru-RU",
-#         "timezone": "UTC",
-#         "client_id": "ru.yandex.searchplugin/7.16 (none none; android 4.4.2)",
-#         "interfaces": {
-#             "screen": {},
-#             "payments": {},
-#             "account_linking": {}
-#         }
-#     },
-#     "session": {
-#         "message_id": 0,
-#         "session_id": "35f9ca30-36dd-4508-bb99-c04f021c0859",
-#         "skill_id": "3529be8e-8eb3-4079-b432-671445c16834",
-#         "user": {
-#             "user_id": "AD1318CEB34263AE62E329C3A4C52E651484515C695D3535986B302D7D3330A7"
-#         },
-#         "application": {
-#             "application_id": "CCB5DE9619835812132D9117146BED632190A27C16D4204DC95B448292F91EFB"
-#         },
-#         "user_id": "CCB5DE9619835812132D9117146BED632190A27C16D4204DC95B448292F91EFB",
-#         "new": 'true'
-#     },
-#     "request": {
-#         "command": "",
-#         "original_utterance": "",
-#         "nlu": {
-#             "tokens": [],
-#             "entities": [],
-#             "intents": {}
-#         },
-#         "markup": {
-#             "dangerous_context": 'false'
-#         },
-#         "type": "SimpleUtterance"
-#     },
-#     "version": "1.0"
-# }
-
-if __name__ == '__main__':
-    app.run()
-    # print(test(request))
