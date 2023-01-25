a31455c8ac5ad82576e64302dfe81e6afcaf2098
Ariestellar <ahdrei47@gmail.com>
2023-01-25 22:32:14 +0300

    Update flask_app.py

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.DEBUG)
 def make_response(text, state=None, buttons=None):
     response = {'response': {'text': text}, 'version': '1.0'}
     if state is not None:
-        response['session']['screen'] = state
+        response['session_state']['screen'] = state
     if buttons:
         response['response']['buttons'] = buttons
     return response
