dade2fb88eb3110c173477352a4fbf05c661248c
Ariestellar <ahdrei47@gmail.com>
2023-01-25 23:45:40 +0300

    Update flask_app.py

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -88,7 +88,7 @@ def main():
     intents = event['request'].get('nlu', {}).get('intents')  # Достаем словарь интентов из запроса
     logging.info('Intents-state:')
     logging.info(intents)
-    state = event['state']['session']['screen']  # Достаем состояние из запроса
+    state = event['state']['session']['ssds']  # Достаем состояние из запроса
     logging.info('CurrentState:')
     logging.info(state)
     if event['session']['new']:
